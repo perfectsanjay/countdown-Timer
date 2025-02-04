499ede069bb93cd62f0b48bb12899723191e4928
Sanjay Singh <sanjay007abd@gmail.com>
2025-02-04 20:52:48 +0530

    add background and style it.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="back-container"></div> {/* Background applied via CSS */}
+
+      <h3 className="title-con">WE'RE LAUNCHING SOON</h3>
+
+      <img className="mountains" src="/images/pattern-hills.svg" alt="mountainsImg" />
+    </div>
+  );
+}
+
+export default App;
